8df4007502aa1ac5d0ed9fd1962eb475613dd62c
aniruddhzaveri <aniruddh.zaveri@tum.de>
2026-05-12 16:47:13 +0200

    update to devops26 and azure from aws

--- server/src/main/java/de/tum/aet/devops25/w05/model/Dish.java
+++
@@ -1,8 +0,0 @@
-package de.tum.aet.devops25.w05.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.List;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record Dish(String name, String dish_type, List<String> labels) {
-}
